aedc69d2378171bb27920309172396707b4d3ab4
Omar Loyo <omarloyo121@gmail.com>
2022-06-23 13:54:15 -0600

    created function to read RFID UIDs

--- nano33-rfid-firebase.ino
+++ nano33-rfid-firebase.ino
@@ -65,3 +65,22 @@ void setup()
 void loop()
 {
 }
+
+void getID()
+{
+    if (!rfid.PICC_IsNewCardPresent())
+        return;
+    if (!rfid.PICC_ReadCardSerial())
+        return;
+
+    String ID = "";
+    for (byte i = 0; i < rfid.uid.size; i++)
+    {
+        ID.concat(String(rfid.uid.uidByte[i] < 0x10 ? " 0" : ""));
+        ID.concat(String(rfid.uid.uidByte[i], HEX));
+    }
+
+    ID.toUpperCase();
+    Serial.println(ID);
+    delay(300);
+}
